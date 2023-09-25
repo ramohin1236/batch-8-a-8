1a4eaf415d4d00b1e7b50a3bbf995382d1f78cf7
ramohin1236 <108376758+ramohin1236@users.noreply.github.com>
2023-09-24 22:47:10 -0600

    donation page complete

--- src/components/Donation/Donationdetails.jsx
+++ src/components/Donation/Donationdetails.jsx
@@ -20,17 +20,27 @@ const Donationdetails = () => {
     //  const {id,Picture}=detailss;
     console.log(detailss, inteId);
     return (
-        <div className=" ">
+        <div className="container mx-auto">
 
 
 <div className="card bg-base-100 shadow-xl  ">
-  <figure className=""><img className="h-96 w-[1200px] " src={detailss.Picture} alt="Shoes" />
+ <div style={{ position: 'relative' }}>
+ <figure className=""><img className="h-96 w-[1300px] " src={detailss.Picture} alt="Shoes" />
   </figure >
-  
+   <div className="h-20 hero-overlay -mt-20 opacity-90 container mx-auto ">
+   <button className="btn w-48" 
+  style={{
+    backgroundColor: detailss.Text_button_bg,
+    color: '#fff', padding: '10px 20px',
+borderRadius: '15px', 
+  }}
+  >{`Donate $${detailss.Price}`}</button>
+   </div>
+ </div>
   
   <div className="card-body container mx-auto">
     <h2 className="card-title">{detailss.Title}</h2>
-    <p className="">If a dog chews shoes whose shoes does he choose?</p>
+    <p className="">{detailss.Description}</p>
     <div className="card-actions justify-end">
       
     </div>
